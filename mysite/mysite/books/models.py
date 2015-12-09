e3f0078a64b38d439a5f5f02bded316c87326a1d
root <root@Joklin.(none)>
2015-12-09 10:10:07 +0800

    1

--- mysite/mysite/books/models.py
+++ mysite/mysite/books/models.py
@@ -11,66 +11,28 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-
-class BooksAuthor(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=40)
-    email = models.CharField(max_length=254)
-
-    class Meta:
-        managed = False
-        db_table = 'books_author'
-
-
-class BooksBook(models.Model):
-    title = models.CharField(max_length=100)
-    publisher = models.ForeignKey('BooksPublisher')
-    publication_date = models.DateField()
-
-    class Meta:
-        managed = False
-        db_table = 'books_book'
-
-
-class BooksAuthors(models.Model):
-    book = models.ForeignKey(BooksBook)
-    author = models.ForeignKey(BooksAuthor)
-
-    class Meta:
-        managed = False
-        db_table = 'books_book_authors'
-        unique_together = (('book_id', 'author_id'),)
-
-
-class BooksPublisher(models.Model):
+class Publisher(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
     city = models.CharField(max_length=60)
     state_province = models.CharField(max_length=30)
     country = models.CharField(max_length=50)
     website = models.CharField(max_length=200)
-
     class Meta:
-        managed = False
-        db_table = 'books_publisher'
         ordering = ['name']
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    email = models.CharField(max_length=254)
 
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher)
+    publication_date = models.DateField()
 
 class ServerStatus(models.Model):
     idserver_status = models.AutoField(primary_key=True)
     status_id = models.IntegerField()
     data = models.TextField()
-
-    class Meta:
-        managed = False
-        db_table = 'server_status'
