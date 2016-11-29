bb236b34a61cb7ce064a1758ac950a7bbcb1aec6
root <root@Jal.(none)>
2016-11-29 17:14:55 +0800

    change to mine_blog

--- bbs/app01/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from app01 import models
-# Register your models here.
-admin.site.register(models.Article)
-admin.site.register(models.Author)
-admin.site.register(models.Tag)
-admin.site.register(models.Classification)
--- bbs/app01/urls.py.bak
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from app01 import views
-from django.views.generic import ListView
-from app01.models import *
-
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name = 'index'),
-)
